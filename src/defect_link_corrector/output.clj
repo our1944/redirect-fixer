8867687be3a830e20621ba1d14b0a14698395b06
Tao Yang <our1944@gmail.com>
2014-11-16 23:46:20 +0100

    wip output

--- src/defect_link_corrector/output.clj
+++ src/defect_link_corrector/output.clj
@@ -1,8 +1,11 @@
 (ns defect-link-corrector.output
   (:require [clojure.data.csv :as csv]))
 
-(defn node-keys [:nid :type :status :title :origin-url :replaced :url-relative :status])
+(def node-keys [:nid :type :status :title :origin-url :replaced :url-relative :status])
 
 (defn write-csv
   [nodes & {:keys [out-writer] :or {out-writer *out*}}]
-  )
+  (let [opts {:separator \;}
+        header (map name node-keys)
+        data (conj (apply list nodes) header)]
+  (csv/write-csv out-writer data opts)))
---
+++ test/defect_link_corrector/output_test.clj
@@ -0,0 +1,5 @@
+(ns defect-link-corrector.output-test
+  (:require [clojure.test :refer :all]
+            [defect-link-corrector.output :refer :all]))
+
+(def nodes [])
